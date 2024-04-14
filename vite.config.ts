9bc4ce9c823f85a9b71f91378102905f74839eb5
Arsen Petrov <pbmspa@gmail.com>
2024-04-14 22:46:25 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://keet5.github.io/pashka-timer/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
